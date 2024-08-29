65dc92a390c333411b883306c514616ada6308ed
marciogsm <marciogsm@gmail.com>
2024-08-29 23:24:04 +0100

    Update zabbix_network_diagnostics.sh

--- zabbix_network_diagnostics.sh
+++ zabbix_network_diagnostics.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
-
 # Script Name: zabbix_network_diagnostics.sh
-# Description: This script diagnoses network issues related to Zabbix proxy communication.
+# Description: Diagnoses network issues related to Zabbix proxy communication.
 # Usage: ./zabbix_network_diagnostics.sh <file> [timeout]
 #        <file> - A file containing a list of IP addresses to check.
 #        [timeout] - (Optional) Timeout value for tcpdump. Default is 60 seconds.
+# Created by: Marcio Moreno
+# Creation Date: 2023-07-29
 
 # Check if the input file is provided
-#if [[ -z $1 ]]; then
-#    echo "Usage: $0 <file> [timeout]"
-#    exit 1
-#fi
-
-# Set timeout value, default is 60 seconds
-TIMEOUT=${2:-180}
+if [[ -z $1 ]]; then
+    echo "Usage: $0 <file> [timeout]"
+    exit 1
+fi
 
 # Set Zabbix Proxy logs path
 ZBXLOG="/var/log/zabbix/zabbix_proxy.log"
@@ -22,24 +20,20 @@ ZBXLOG="/var/log/zabbix/zabbix_proxy.log"
 HOST=$(hostname)
 
 # Filter hosts by Zabbix Proxy and prepare file
-while IFS=";" read -r concat cust hub host ok addr os mrdmon c proxy e status g h i j k l obs; do
-        echo "$cust;$addr;$proxy;$host;$os;$status;$obs"
-done <<<  $(grep -i $(hostname) /home/mgmoreno/Controle) >  /home/mgmoreno/$(hostname)
+grep -i "$(hostname)" /home/mgmoreno/Controle | while IFS=";" read -r concat cust hub host ok addr os mrdmon addrproxy proxy e status g h i j k l obs gestao; do
+    echo "$cust;$addr;$proxy;$host;$os;$status;$obs;$gestao;$addrproxy"
+done > /home/mgmoreno/$(hostname)
 
 # Print header
-echo -e "ADDR;ZBX_ERROR_CODE;ZBX_OUT;ICMP;Trace;ZBXProxy received connections on port 10051?;Host listening on port 10050?;ZBXProxy logs contain errors?;Issue;Gama;ZBXProxy;Cust;Host;OS;Status;OBS"
+echo -e "Host;ADDR;ZBX_ERROR_CODE;ZBX_OUT;ICMP;Trace;ZBXProxy received connections on port 10051?;Host listening on port 10050?;ZBXProxy logs contain errors?;Issue;Gama;ZBXProxy;ZBXProxyAddr;Cust;OS;Status;OBS;Gestao"
 
 # Loop through each address in the input file
-while IFS=";" read -r cust addr proxy host os status obs; do
+while IFS=";" read -r cust addr proxy host os status obs gestao addrproxy; do
     # Initialize ISSUE flag
     ISSUE="No"
-# while IFS=";" read -r addrworked proxyworked; do
-# if [[ "$addrworked$proxyworked" == "$addr$proxy" ]]; then
-#        echo -e "$addr;$ZBX_ERROR_CODE;$ZBX_OUT;$ICMP;$TRACE;$TCPDUMP_STATUS;$NMAP_STATUS;$LOG_STATUS;PreviouslyWorked;$GAMA;$proxy;$cust;$host;$os;$status;$obs"
-#        continue
-# else
+
     # Isolate network
-    GAMA=$( echo $addr | sed -e 's/\.[0-9]\+$/\.0\/24/g')
+    GAMA=$(echo "$addr" | sed -e 's/\.[0-9]\+$/\.0\/24/g')
 
     # ICMP check
     ICMP=$(ping -W 0.5 -c2 "$addr" | grep -Eo '[0-9]{1,3}% \w+ \w+')
@@ -51,45 +45,51 @@ while IFS=";" read -r cust addr proxy host os status obs; do
     else
         TRACE="Tracert ok"
     fi
+    
+# Set timeout value, default is 60 seconds
+TIMEOUT=${2:-60}
 
-    # Zabbix agent check
-    ZBX_OUT=$(zabbix_get -t 5 -s "$addr" -k agent.hostname 2>&1 | awk 'NR==1 {output=$0} NR>1 {output=output"|"$0} END {print output}')
-    ZBX_ERROR_CODE="$?"
-    if [[ $ZBX_ERROR_CODE = "1" ]]; then
+    # Tcpdump check if status is not equal to "Instalado"
+    if [[ "$status" != "Instalado" ]]; then
+        TCPDUMP=$(sudo timeout ${TIMEOUT} tcpdump -c1 -nnn -vvv -i any host "$addr" and port 10051 2>&1)
+        if echo "$TCPDUMP" | grep -q "$addr"; then
+            TCPDUMP_STATUS="Zabbix Proxy received packets on port 10051"
+        else
+            TCPDUMP_STATUS="Zabbix Proxy no packets received on port 10051"
             ISSUE="Yes"
+        fi
+    else
+        TCPDUMP_STATUS="Zabbix Proxy received packets on port 10051"
     fi
 
-    # Tcpdump check
-if [[ $status != "Instalado" ]]; then
-    TCPDUMP=$(sudo timeout ${TIMEOUT} tcpdump -c1 -nnn -vvv -i any host "$addr" and port 10051 2>&1)
-    if echo "$TCPDUMP" | grep -q "$addr"; then
-        TCPDUMP_STATUS="Zabbix Proxy received packets on port 10051"
-    else
-        TCPDUMP_STATUS="Zabbix Proxy no packets received on port 10051"
-        ISSUE="Yes"
-    fi
-fi
     # Nmap port check
     NMAP=$(sudo nmap -Pn "$addr" -p10050 | awk '/tcp/ {print $2}')
     if [[ $NMAP =~ "closed" || $NMAP =~ "filtered" || -z $NMAP ]]; then
         NMAP_STATUS="$NMAP Port 10050 on customer host"
         ISSUE="Yes"
+        timeout="1"
     else
         NMAP_STATUS="$NMAP Port 10050 on customer host"
+        timeout="10"
+    fi
+
+    # Zabbix agent check
+    ZBX_OUT=$(zabbix_get -t "$timeout" -s "$addr" -k agent.hostname 2>&1 | awk -F': ' 'NR==1 {output=$2} NR>1 {output=output"|" $2} END {print output}')
+    ZBX_ERROR_CODE="$?"
+    if [[ $ZBX_ERROR_CODE = "1" ]]; then
+        ISSUE="Yes"
     fi
 
     # Zabbix proxy log check
-
     IP=$(echo "$addr" | sed 's/\./\\./g')
-    LOG=$( grep $(date '+%Y%m%d') $ZBXLOG | grep -m1 -w "$IP")
+    LOG=$(grep "$(date '+%Y%m%d')" "$ZBXLOG" | grep -m1 -w "$IP")
     if [[ $? -eq 0 ]]; then
-        LOG_STATUS="Found ERROR Zabbix Proxy LOG - $LOG"
+        LOG_STATUS="Found ERROR in Zabbix Proxy LOG - $LOG"
     else
-        LOG_STATUS="Not Found ERROR Zabbix Proxy LOG"
+        LOG_STATUS="No errors found in Zabbix Proxy LOG"
     fi
 
     # Output results for the current address
-    echo -e "$addr;$ZBX_ERROR_CODE;$ZBX_OUT;$ICMP;$TRACE;$TCPDUMP_STATUS;$NMAP_STATUS;$LOG_STATUS;$ISSUE;$GAMA;$proxy;$cust;$host;$os;$status;$obs"
-#fi
-#done < <(cat /home/mgmoreno/PreviouslyWorked)
+    echo -e "$host;$addr;$ZBX_ERROR_CODE;$ZBX_OUT;$ICMP;$TRACE;$TCPDUMP_STATUS;$NMAP_STATUS;$LOG_STATUS;$ISSUE;$GAMA;$proxy;$addrproxy;$cust;$os;$status;$obs;$gestao"
+
 done < "/home/mgmoreno/$(hostname)"
